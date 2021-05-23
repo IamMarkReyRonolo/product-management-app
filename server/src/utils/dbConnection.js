9bfc9d0e9d7a831cbef1cfe8a8b73aafb2a26c61
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2021-05-24 01:30:11 +0800

    modified dbConnection

--- server/src/utils/dbConnection.js
+++ server/src/utils/dbConnection.js
@@ -2,6 +2,18 @@ const { Sequelize } = require("sequelize");
 
 require("dotenv").config();
 
-const db = new Sequelize(process.env.DATABASE_URL);
+const db = new Sequelize({
+	database: process.env.DATABASE_NAME,
+	username: process.env.DATABASE_USER,
+	password: process.env.DATABASE_PASSWORD,
+	host: process.env.DATABASE_HOST,
+	port: 5432,
+	dialect: "postgres",
+	dialectOptions: {
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
+});
 
 module.exports = db;
