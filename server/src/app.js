f7778a7d1e483cdd92599ab54b8272bebe3ad28c
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2021-05-24 02:11:21 +0800

    modified

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const auth = require("./controllers/auth");
 const app = express();
 
 // MIDDLEWARES
-app.use(__dirname + "/uploads", express.static(__dirname + "/uploads"));
+app.use(express.static(__dirname + "/uploads"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
