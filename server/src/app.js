93814dbaf014df63b848f5c4471264a90560d4f5
IamMarkReyRonolo <ronolomarkrey@gmail.com>
2021-05-24 02:56:31 +0800

    modified

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const app = express();
 
 // MIDDLEWARES
-app.use(express.static("uploads"));
+app.use(express.static(path.join(__dirname, "uploads")));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
